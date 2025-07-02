d459e36f986f85981c6f684ba15354da8372f84e
NaveenMahajan <naveenmahajan12345678@gmail.com>
2025-07-02 15:48:27 +0530

    link

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,5 @@
 import { Briefcase } from 'lucide-react';
-
+import { FaLink } from "react-icons/fa";
 const Experience = () => {
   const experienceData = [
     {
@@ -34,7 +34,12 @@ const Experience = () => {
             >
               <div className="mb-4">
                 <h3 className="text-xl font-bold text-purple-700">{exp.title}</h3>
-                <p className="text-gray-800 font-medium">{exp.company}</p>
+               <div className='flex gap-5'>
+                 <p className="text-gray-800 font-medium">{exp.company} </p>
+             <a href='https://www.linkedin.com/company/telentrisetechnokrate/posts/?feedView=all'>
+                <FaLink  size={20} className='text-purple-500'/>
+             </a>
+               </div>
                 <p className="text-sm text-gray-500">{exp.date}</p>
                 <p className="mt-1 text-gray-600">{exp.description}</p>
               </div>
